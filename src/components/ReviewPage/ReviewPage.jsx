9ff4716681cf737465e53d1c83329997aa6fe88c
Tina Tram <tinatram07@gmail.com>
2023-02-04 11:50:30 -0600

    created handleSubmit function for submit button on review page, set up post request and imported axios

--- src/components/ReviewPage/ReviewPage.jsx
+++ src/components/ReviewPage/ReviewPage.jsx
@@ -1,11 +1,21 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import axios from 'axios';
 
 function ReviewPage() {
     const feelingScore = useSelector(store => store.feelingScore);
     const understandScore = useSelector(store => store.understandScore);
     const supportScore = useSelector(store => store.supportScore);
     const comments = useSelector(store => store.comments);
+
+    const handleSubmit = () => {
+        // handle post request and history.push(/newpage)
+        axios.post('/feedback', newFeedback)
+        .then(response => {
+            console.log('successful post', response)
+        getFeedback();
+    })
+    }
 
     return (
         <>
@@ -14,7 +24,7 @@ function ReviewPage() {
             <p>Understanding: {understandScore}</p>
             <p>Support: {supportScore}</p>
             <p>Comments: {comments}</p>
-            <button>Submit</button>
+            <button onClick = {() => handleSubmit()} className="button">Submit</button>
         </>
     )
 }
